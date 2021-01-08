a85e748b5ff7d5cbd9052c950a06c4e805b7e64a
William Bryan <wwbryan00@gmail.com>
2021-01-08 11:22:33 -0800

    determine lat and lon of parks nearby

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-var apiKey = "c8cbf2d95a5dea77ab7ea30913acbe6d";
+var weatherApiKey = "c8cbf2d95a5dea77ab7ea30913acbe6d";
+var googlePlacesApiKey = "AIzaSyBkWn72ID7mx-gdIlXW5iUCHaT8-O-3lBA";
 
 $(document).ready(function(){
 
@@ -19,13 +20,13 @@ $('.INFO-FIELD-SEARCH').on("click", function() {
 
 function checkWeather(zipCode) {
     
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q="+ zipCode + "&appid=" + apiKey;
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q="+ zipCode + "&appid=" + weatherApiKey;
 
     $.ajax ({
         url: queryURL,
         method: "GET"
     }).then(function(response) { 
-        console.log(response);
+        console.log("Weather API Response: " + response);
         var weatherDesc = reponse.weather.description;
         if (weatherDesc === "clear sky"){
             locatePark(zipCode);
@@ -36,6 +37,29 @@ function checkWeather(zipCode) {
     });
 }
 
+function locatePark(zipCode) {
+    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+" + zipCode + "&key=" + googlePlacesApiKey;
+    var latNlon = [];
+
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) { 
+        console.log("Google Map Response: " + response);
+
+        // Construct a map making object containing all information needed
+        for (var i=0; i < 5; i++){
+            var tempOBJ = {lat: "",lon: ""}
+            tempOBJ.lat = response.result[i].geometry.location.lat;
+            tempOBJ.lon = response.result[i].geometry.location.lon;
+            latNlon.push(tempOBJ);
+        }
+
+        drawMap(latNlon);
+    });
+}
+
+
 function generateModal(str) {
     if (str === "error"){
         // incorrect zip code case
@@ -43,6 +67,8 @@ function generateModal(str) {
 
     if (str === "bad") {
         // bad weather case
+        // Prompt user yes or no if they want to visit a coffee shop instead
+        // if yes execute locateShop();
     }
 }
 
